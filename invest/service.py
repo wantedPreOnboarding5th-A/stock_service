672a19a9c0a8681bf35eb1067c1d9bc170d1c0d0
JunH <101803254+Blaze-F@users.noreply.github.com>
2022-11-02 01:14:08 +0900

    [TEST] 테스트 관련 MockRepo 생성

--- invest/service.py
+++ invest/service.py
@@ -2,28 +2,27 @@ from invest.repository import AbstractInvestInfoRepo, AccountRepo, InvestInfoRep
 from user.repository import UserRepo
 
 
-invest_info_repo = InvestInfoRepo()
-account_repo = AccountRepo()
-stock_repo = StockRepo()
-user_repo = UserRepo()
+
 
 
 class InvestInfoManagementSerivice:
-    def __init__(self, repo:AbstractInvestInfoRepo) -> None:
-        self.repo = repo
+    def __init__(self, invest_info_repo:AbstractInvestInfoRepo,user_repo:AbstractInvestInfoRepo) -> None:
+        self.invest_info_repo = invest_info_repo
+        self.user_repo = user_repo
         
     def get_invest_info(self, account_number: int) -> dict:
-        invest_info_list = invest_info_repo.find_by_account_number(account_number=account_number)
-        user_info = user_repo.get_by_account_number(account_number=account_number)
+        invest_info_list = self.invest_info_repo.find_by_account_number(account_number=account_number)
+        user_info = self.user_repo.get_by_account_number(account_number=account_number)
         # filter, .select_related, 자동 캐싱된다.
         all_assets = 0
         for i in invest_info_list:
             all_assets = all_assets + i["amount"] * i["current_price"]
 
-        # 1인 1계좌, 계좌에서 이름 가져오기.
-        account_name = invest_info_list[1]["name"]
-        brokerage = invest_info_list[1]["brokerage"]
-        account_number = invest_info_list[1]["number"]
+        # 계좌정보
+        account_name = invest_info_list[1]["account"]["name"]
+        brokerage = invest_info_list[1]["account"]["brokerage"]
+        account_number = invest_info_list[1]["account"]["number"]
+        # 유저정보
         user_name = user_info["name"]
 
         data = {
