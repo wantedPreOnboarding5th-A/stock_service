54d0c19e6f20c9c32603a04daa1625b1b9bf31b2
ehdrb92 <kcj920407@gmail.com>
2022-11-01 13:42:37 +0900

    [feat] 보유 종목 API 작업중

--- invest/repository.py
+++ invest/repository.py
@@ -95,5 +95,14 @@ class StockRepo(AbstractStockRepo):
     def get(self, stock_id: int) -> dict:
         try:
             return self.serializer(self.model.objects.get(id=stock_id))
+            res = []
+
+            for account in accout_id:
+                created = self.serializer(
+                    self.model.objects.prefetch_related("investinfo_set").filter(accout_id=account)
+                ).data
+                res.append(created)
+
+            return res
         except self.model.DoesNotExist:
             raise NotFoundError
