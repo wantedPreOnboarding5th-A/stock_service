827ae042349c9e9e0c2886341dd1d4430606deed
ehdrb92 <kcj920407@gmail.com>
2022-11-02 18:49:38 +0900

    [feat] 스웨거 추가 적용

--- invest/views.py
+++ invest/views.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
-from invest.serializers import InvestInfoDetailResSchema, InvestInfoResSchema
+from invest.serializers import (
+    InvestInfoDetailResSchema,
+    InvestInfoResSchema,
+    StockHeldListResSchema,
+)
 from invest.service import StockService
 from drf_yasg.utils import swagger_auto_schema
 from provider.auth_provider import AuthProvider
@@ -20,19 +24,15 @@ invest_management_service = InvestInfoManagementSerivice(
     invest_info_repo=invest_info_repo, user_repo=user_repo
 )
 
-@swagger_auto_schema(
-    method="get",
-    responses={200:InvestInfoResSchema}
-)
+
+@swagger_auto_schema(method="get", responses={200: InvestInfoResSchema})
 @api_view(["GET"])
 @parser_classes([JSONParser])
 def get_invest_info(request, account_number: str):
     return JsonResponse(invest_management_service.get_invest_info(account_number=account_number))
 
-@swagger_auto_schema(
-    method="get",
-    responses={200:InvestInfoDetailResSchema}
-)
+
+@swagger_auto_schema(method="get", responses={200: InvestInfoDetailResSchema})
 @api_view(["GET"])
 @parser_classes([JSONParser])
 def get_invest_detail(request, account_number: int):
@@ -40,17 +40,12 @@ def get_invest_detail(request, account_number: int):
     return JsonResponse(invest_management_service.get_invest_detail(account_number=account_number))
 
 
-# 보유 종목 요청 API
+@swagger_auto_schema(
+    method="get",
+    responses={200: StockHeldListResSchema},
+)
 @api_view(["GET"])
 @parser_classes([JSONParser])
-@swagger_auto_schema(
-    responses={
-        "name": "name",
-        "group": "group",
-        "evaluation_amount": "evaluation_amount",
-        "isin_number": "isin_number",
-    },
-)
 def get_list(request):
     auth_token = request.META.get("HTTP_AUTHORIZATION", None)
     decoded = auth_provider._decode(token=auth_token)
